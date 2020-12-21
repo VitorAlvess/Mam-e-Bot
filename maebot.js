2ffdf2c303ec9a36526e50e036ae35f792a13b4e
VitorAlvess <53319601+VitorAlvess@users.noreply.github.com>
2020-12-21 10:14:21 -0300

    Correção da maioria dos erros do !animesrandom

--- maebot.js
+++ maebot.js
@@ -1,9 +1,9 @@
 const Discord = require("discord.js"); //baixar a lib
 const client = new Discord.Client();
-const token = require("./config.js");
-const token2 = require("./configapi2.js");
+const token = require("./config.js"); //chave da api do discord
+const token2 = require("./configapi2.js");  //chave da api de busca de imagem
 
-const palavras = require("./palavras.js");
+const palavras = require("./palavras.js"); //Filtro de palavras
 const request = require("request");
 const invite = "https://discord.gg/HAhVwJYQaC";
 const unirest = require("unirest");
@@ -14,55 +14,9 @@ const unirest = require("unirest");
 client.on("ready", () => {
   console.log("Bot Mãe On");
   client.user.setActivity(`Cuidando de alguns pestinhas (Crinças)`);
-  // var a = client.channels.get('765383157083930632')
-  // console.log(a)
-  // 765383157083930632
-  // client.user.setGame("fala")
+
 });
-// client.on("guildMemberAdd", member => {
-//     member.guild.channels.get('765383157083930629').send(member.user.username + 'Bem vindo!');
-//     console.log(member)
-//     member.send('Opa teste teste')
-// })
-// client.on('raw', async dados => {
 
-// })
-//Add cargos por reação
-// client.on('raw', async dados => {
-
-//     if(dados.t !== "MESSAGE_REACTION_ADD" && dados.t !== "MESSAGE_REACTION_REMOVE") return
-//     if(dados.d.message_id != "783368730464878604") return
-
-//     let servidor = client.guilds.get("765383156953120778")
-//     let membro = servidor.members.get(dados.d.user_id)
-
-//     var cargo1 = servidor.roles.get('783371672190713916'),
-//         cargo2 = servidor.roles.get('783371771738980392')
-
-//     if(dados.t === "MESSAGE_REACTION_ADD"){
-//         if(dados.d.emoji.name === "😄"){
-//             if(membro.roles.has(cargo1)) return
-//             membro.addRole(cargo1)
-//             console.log('Cargo 1')
-//         }else if(dados.d.emoji.name === "😠"){
-//             if(membro.roles.has(cargo2)) return
-//             membro.addRole(cargo2)
-//             console.log('Cargo 2')
-//         }
-//     }
-//     if(dados.t === "MESSAGE_REACTION_REMOVE"){
-//         if(dados.d.emoji.name === "😄"){
-//             if(membro.roles.has(cargo1)) return
-//             membro.removeRole(cargo1)
-//             console.log(' remove Cargo 1')
-//         }else if(dados.d.emoji.name === "😠"){
-//             if(membro.roles.has(cargo2)) return
-//             membro.removeRole(cargo2)
-//             console.log('remove Cargo 2')
-//         }
-//     }
-
-// })
 
 client.on("message", (msg) => {
   // https://appanimeplus.tk/api-animesbr-10.php?info=ID_DO_ANIME
@@ -76,9 +30,18 @@ client.on("message", (msg) => {
           console.log("error:", error);
           console.log("statusCode:", response && response.statusCode);
           let content = JSON.parse(body);
+          try{
+            var pesquisa = content[0].category_name
+          }
+        
+          catch (err) {
+            console.log(err);
 
-          var pesquisa = content[0].category_name;
-          var texto = `**${pesquisa.toString()}**\n${content[0].category_description.toString()}`
+            return  msg.channel.send("Algo deu errado, Tente novamente");
+          }
+
+          
+          var texto = `**${pesquisa.toString()}**\n${content[0].category_description.toString()}\n\n${content[0].category_genres}`
           msg.channel.send("```" + texto + "```");
           var req = unirest(
             "GET",
@@ -94,24 +57,35 @@ client.on("message", (msg) => {
 
           req.headers({
             "x-rapidapi-key":
-              token2,
+              token2, //API <
             "x-rapidapi-host":
               "contextualwebsearch-websearch-v1.p.rapidapi.com",
             useQueryString: true,
           });
 
           req.end(function (res) {
-            if (res.error) throw new Error(msg.channel.send("Aconteceu um erro, Tente Novamente"));
-
-            msg.channel.send(res.body.value[0].url);
+            
+            // if (res.error) throw new Error(msg.channel.send("Aconteceu um erro, Tente Novamente"));
+           
+            try{
+              console.log(res.body.value[0].url);
+              msg.channel.send(res.body.value[0].url);
+            }
+            catch(err){
+              console.log(err);
+              msg.channel.send('Um erro ocorreu, Tente Novamente');
+            }
+          
           });
         }
       );
     } catch (err) {
-      msg.channel.send("Aconteceu um erro, Tente Novamente");
+      console.log(err);
+
+      return msg.channel.send("Aconteceu um erro, Tente Novamente");
     }
   }
-  if (msg.content.toLowerCase() == "!anime") {
+  if (msg.content.toLowerCase() == "!animestemporada") {
     request(
       "https://appanimeplus.tk/api-animesbr-10.php?latest",
       function (error, response, body) {
@@ -123,9 +97,20 @@ client.on("message", (msg) => {
           texto += `${conteudo[index].title.toString()} \n`;
         }
         msg.channel.send("```" + texto + "```");
-        //   console.log('body:', conteudo); // Print the HTML for the Google homepage.
+       
       }
     );
+  }
+  if (msg.content.toLowerCase() == "!help" || msg.content.toLowerCase() == '!comandos' ) {
+    let comandos = `
+    *Comandos*
+    !link -> Envia o convite do servidor no seu privado
+    !img -> Envia uma imagem aleatoria
+    !animestemporada -> Mostra a lista de animes da temporada
+    !animerandom -> Mostra um anime aleatorio
+    `
+    msg.reply("```" + comandos + "```");
+
   }
   if (msg.content.toLowerCase() == "!img") {
     let a = `https://source.unsplash.com/random/2000x2000?sig=${
